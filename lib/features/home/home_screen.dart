ca6751cf2acdc11e05a78b0ebed0f25a85e9a9ad
fairytaib <fairytaib@gmail.com>
2026-04-08 08:55:40 +0200

    build card

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -56,13 +56,11 @@ class Header extends StatelessWidget {
 }
 
 class CardSceleton extends StatelessWidget {
-  final List<Widget> child;
-  final Color backgroundColor;
+  final List<Widget> cards;
 
   const CardSceleton({
     super.key,
-    required this.child,
-    required this.backgroundColor,
+    required this.cards,
   });
 
   @override
@@ -85,40 +83,21 @@ class CardSceleton extends StatelessWidget {
           width: double.infinity,
           child: Stack(
             children: [
-              Positioned(
-                top: 20,
-                left: 10,
-                right: 10,
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: MainColors.emerald,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
+              if(cards.length > 2)
+                Positioned(
+                  top: 20,
+                  left: 10,
+                  right: 10,
+                  child: cards[2],
                 ),
-              ),
-              Positioned(
-                top: 10,
-                left: 5,
-                right: 5,
-                child: Container(
-                  width: double.infinity,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: MainColors.brown,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
+              if(cards.length > 1)
+                Positioned(
+                  top: 10,
+                  left: 5,
+                  right: 5,
+                  child: cards[1],
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 200,
-                decoration: BoxDecoration(
-                  color: MainColors.gold,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
+              cards[0]
             ],
           ),
         ),
@@ -129,7 +108,12 @@ class CardSceleton extends StatelessWidget {
 
 class Card extends StatelessWidget {
   final Widget child;
-  const Card({super.key, required this.child});
+  final Color backgroundColor;
+  const Card({
+    super.key,
+    required this.child,
+    required this.backgroundColor,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -137,7 +121,7 @@ class Card extends StatelessWidget {
       width: double.infinity,
       height: 200,
       decoration: BoxDecoration(
-        color: MainColors.emerald,
+        color: backgroundColor,
         borderRadius: BorderRadius.circular(16),
       ),
       child: child,
@@ -148,7 +132,6 @@ class Card extends StatelessWidget {
 class TagData {
   final String label;
   final IconData? icon; // Optionales Icon
-
   TagData({required this.label, this.icon});
 }
 
@@ -179,24 +162,40 @@ class CardContent extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(20.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Icon(icon, size: 32, color: const Color.fromARGB(185, 255, 255, 255)),
-          SizedBox(height: 8),
+          Spacer(),
           Text(
             title,
             style: TextStyle(
+              fontFamily: 'DM Sans',
               fontSize: 14,
               fontWeight: FontWeight.bold,
               color: titleColor,
             ),
           ),
           SizedBox(height: 8),
-          Text(value, style: TextStyle(fontSize: 18, color: valueColor)),
-          SizedBox(height: 4),
-          Text(comment, style: TextStyle(fontSize: 12, color: commentColor)),
-          SizedBox(height: 8),
+          Text(
+            value.split(" ")[0],
+            style: TextStyle(
+              fontFamily: 'Cormorant Garamond',
+              fontSize: 18,
+              color: valueColor
+              )
+            ),
+            Text(
+            value.contains('/') ? '/ ${value.split('/')[1]}' : '',
+            style: TextStyle(
+              fontFamily: 'Cormorant Garamond',
+              fontSize: 12,
+              color: valueColor
+              )
+            ),
+          Spacer(),
+          Text(comment, style: TextStyle(fontFamily: 'Cormorant Garamond', fontSize: 12, color: commentColor)),
+          Spacer(),
           Wrap(
             spacing: 8,
             runSpacing: 4,
@@ -210,14 +209,18 @@ class CardContent extends StatelessWidget {
                 child: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
+                    Text(
+                      tag.label,
+                      style: TextStyle(
+                        fontFamily: 'DM Sans',
+                        fontSize: 12,
+                        color: Colors.white
+                      ),
+                    ),
                     if (tag.icon != null) ...[
                       Icon(tag.icon, size: 12, color: Colors.white),
                       SizedBox(width: 4),
                     ],
-                    Text(
-                      tag.label,
-                      style: TextStyle(fontSize: 12, color: Colors.white),
-                    ),
                   ],
                 ),
               );
@@ -226,6 +229,21 @@ class CardContent extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class InteractiveCardStack extends StatefulWidget {
+  final List<Widget> cards;
+  const InteractiveCardStack({super.key, required this.cards});
+
+  @override
+  State<InteractiveCardStack> createState() => _InteractiveCardStackState();
+}
+
+class _InteractiveCardStackState extends State<InteractiveCardStack> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
 
@@ -241,7 +259,65 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [const Header(), SizedBox(height: 32), const Cards()],
+            children: [
+              const Header(), 
+              SizedBox(height: 32),
+              CardSceleton(
+                cards: [
+                  Card(
+                    backgroundColor: MainColors.emerald,
+                    child: CardContent(
+                      icon: Icons.nightlight_round,
+                      title: 'Deen - Spiritualität',
+                      titleColor: Colors.white,
+                      value: '4 /5 Gebete',
+                      valueColor: Colors.white,
+                      comment: '1 Dua ausstehend',
+                      commentColor: Colors.white70,
+                      tags: [
+                        TagData(label: 'Fajr', icon: Icons.check),
+                        TagData(label: 'Dhuhr', icon: Icons.check),
+                        TagData(label: 'Asr', icon: Icons.check),
+                        TagData(label: 'Maghrib', icon: Icons.check),
+                      ],
+                    ),
+                    
+                  ),
+                  Card(
+                    backgroundColor: MainColors.brown,
+                    child: CardContent(
+                      icon: Icons.attach_money,
+                      title: 'Finanzen - Haushalt',
+                      titleColor: Colors.white,
+                      value: '342 €',
+                      valueColor: Colors.white,
+                      comment: 'Budget noch Verfügbar',
+                      commentColor: Colors.white70,
+                      tags: [
+                        TagData(label: 'Einnahmen +€2800'),
+                        TagData(label: 'Gestern: €450 Ausgaben'),
+                      ],
+                    ),
+                    
+                  ),
+                  Card(
+                    backgroundColor: MainColors.purple,
+                    child: CardContent(
+                      icon: Icons.local_restaurant_rounded,
+                      title: 'Dates - Ehe',
+                      titleColor: Colors.white,
+                      value: 'Freitag 12.04',
+                      valueColor: Colors.white,
+                      comment: 'Erinnerung für Date am Freitag',
+                      commentColor: Colors.white70,
+                      tags: [
+                        TagData(label: 'Nächstes Date'),
+                        TagData(label: 'Picknick im Park'),
+                      ],
+                    ),
+                    
+                  ),
+              )],
           ),
         ),
       ),
--- lib/shared/style_constants.dart
+++ lib/shared/style_constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MainColors {
   static const Color emerald = Color(0xFF0F6E56);
+  static const Color purple = Color(0xFF534AB7);
   static const Color gold = Color(0xFFBA7517);
   static const Color ink = Color(0xFF2C2C2A);
   static const Color brown = Color(0xFF8B4513);
