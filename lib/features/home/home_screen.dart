0c5e5e47ec5d117b624e2e990c65e53a8d49f120
fairytaib <fairytaib@gmail.com>
2026-04-10 12:45:20 +0200

    Build Memorie section

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -476,7 +476,7 @@ class _InformationCenterState extends State<InformationCenter> {
 class LastMemories extends StatefulWidget {
   final IconData icon;
   final String title;
-  final String date;
+  final DateTime date;
   final String tag;
   final String paidMoney;
 
@@ -554,6 +554,34 @@ class _LastMemoriesState extends State<LastMemories> {
   }
 }
 
+class LastMemoriesSection extends StatelessWidget {
+  final List<Widget> children;
+  const LastMemoriesSection({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Letze Erlebnisse',
+          style: TextStyle(
+            fontFamily: 'DM Sans',
+            fontSize: 18,
+            color: Color.fromARGB(185, 255, 255, 255),
+          ),
+        ),
+        const SizedBox(height: 8),
+        for (var child in children) ...[
+          child,
+          const SizedBox(height: 4),
+        ], // max 3 children pls
+      ],
+    );
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -561,100 +589,127 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Header(),
-              SizedBox(height: 32),
-              InteractiveCardStack(
-                cards: [
-                  Card(
-                    backgroundColor: MainColors.emerald,
-                    child: CardContent(
-                      icon: Icons.nightlight_round,
-                      title: 'Deen - Spiritualität',
-                      titleColor: Colors.white,
-                      value: '4 /5 Gebete',
-                      valueColor: Colors.white,
-                      comment: '1 Dua ausstehend',
-                      commentColor: Colors.white70,
-                      tags: [
-                        TagData(label: 'Fajr', icon: Icons.check),
-                        TagData(label: 'Dhuhr', icon: Icons.check),
-                        TagData(label: 'Asr', icon: Icons.check),
-                        TagData(label: 'Maghrib', icon: Icons.check),
-                      ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Header(),
+                SizedBox(height: 32),
+                InteractiveCardStack(
+                  cards: [
+                    Card(
+                      backgroundColor: MainColors.emerald,
+                      child: CardContent(
+                        icon: Icons.nightlight_round,
+                        title: 'Deen - Spiritualität',
+                        titleColor: Colors.white,
+                        value: '4 /5 Gebete',
+                        valueColor: Colors.white,
+                        comment: '1 Dua ausstehend',
+                        commentColor: Colors.white70,
+                        tags: [
+                          TagData(label: 'Fajr', icon: Icons.check),
+                          TagData(label: 'Dhuhr', icon: Icons.check),
+                          TagData(label: 'Asr', icon: Icons.check),
+                          TagData(label: 'Maghrib', icon: Icons.check),
+                        ],
+                      ),
                     ),
-                  ),
-                  Card(
-                    backgroundColor: MainColors.brown,
-                    child: CardContent(
-                      icon: Icons.attach_money,
-                      title: 'Finanzen - Haushalt',
-                      titleColor: Colors.white,
-                      value: '342 €',
-                      valueColor: Colors.white,
-                      comment: 'Budget noch Verfügbar',
-                      commentColor: Colors.white70,
-                      tags: [
-                        TagData(label: 'Einnahmen +€2800'),
-                        TagData(label: 'Gestern: €450 Ausgaben'),
-                      ],
+                    Card(
+                      backgroundColor: MainColors.brown,
+                      child: CardContent(
+                        icon: Icons.attach_money,
+                        title: 'Finanzen - Haushalt',
+                        titleColor: Colors.white,
+                        value: '342 €',
+                        valueColor: Colors.white,
+                        comment: 'Budget noch Verfügbar',
+                        commentColor: Colors.white70,
+                        tags: [
+                          TagData(label: 'Einnahmen +€2800'),
+                          TagData(label: 'Gestern: €450 Ausgaben'),
+                        ],
+                      ),
                     ),
-                  ),
-                  Card(
-                    backgroundColor: MainColors.purple,
-                    child: CardContent(
+                    Card(
+                      backgroundColor: MainColors.purple,
+                      child: CardContent(
+                        icon: Icons.local_restaurant_rounded,
+                        title: 'Dates - Ehe',
+                        titleColor: Colors.white,
+                        value: 'Freitag 12.04',
+                        valueColor: Colors.white,
+                        comment: 'Erinnerung für Date am Freitag',
+                        commentColor: Colors.white70,
+                        tags: [
+                          TagData(label: 'Nächstes Date'),
+                          TagData(label: 'Picknick im Park'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                QuickActionsSection(
+                  children: [
+                    QuickAction(
+                      backgroundColor: const Color.fromARGB(255, 38, 199, 124),
+                      icon: Icons.menu_book,
+                      text: 'Dua hinzufügen',
+                    ),
+                    QuickAction(
+                      backgroundColor: const Color(0xFFE79231),
+                      icon: Icons.add,
+                      text: 'Ausgaben hinzufügen',
+                    ),
+                    QuickAction(
+                      backgroundColor: const Color.fromARGB(255, 164, 49, 231),
                       icon: Icons.local_restaurant_rounded,
-                      title: 'Dates - Ehe',
-                      titleColor: Colors.white,
-                      value: 'Freitag 12.04',
-                      valueColor: Colors.white,
-                      comment: 'Erinnerung für Date am Freitag',
-                      commentColor: Colors.white70,
-                      tags: [
-                        TagData(label: 'Nächstes Date'),
-                        TagData(label: 'Picknick im Park'),
-                      ],
+                      text: 'Date planen',
                     ),
-                  ),
-                ],
-              ),
-              QuickActionsSection(
-                children: [
-                  QuickAction(
-                    backgroundColor: const Color.fromARGB(255, 38, 199, 124),
-                    icon: Icons.menu_book,
-                    text: 'Dua hinzufügen',
-                  ),
-                  QuickAction(
-                    backgroundColor: const Color(0xFFE79231),
-                    icon: Icons.add,
-                    text: 'Ausgaben hinzufügen',
-                  ),
-                  QuickAction(
-                    backgroundColor: const Color.fromARGB(255, 164, 49, 231),
-                    icon: Icons.local_restaurant_rounded,
-                    text: 'Date planen',
-                  ),
-                  QuickAction(
-                    backgroundColor: const Color(0xFFE79231),
-                    icon: Icons.pie_chart_rounded,
-                    text: 'Zakat rechnen',
-                  ),
-                ],
-              ),
-              InformationCenter(
-                backgroundColor: MainColors.ink,
-                icon: Icons.warning_amber_rounded,
-                title: 'Erinnerung',
-                value: 'Zakat fällig in 3 Tagen',
-                comment: 'Berechne deine Zakat rechtzeitig',
-              ),
-            ],
+                    QuickAction(
+                      backgroundColor: const Color(0xFFE79231),
+                      icon: Icons.pie_chart_rounded,
+                      text: 'Zakat rechnen',
+                    ),
+                  ],
+                ),
+                InformationCenter(
+                  backgroundColor: MainColors.ink,
+                  icon: Icons.warning_amber_rounded,
+                  title: 'Erinnerung',
+                  value: 'Zakat fällig in 3 Tagen',
+                  comment: 'Berechne deine Zakat rechtzeitig',
+                ),
+                LastMemoriesSection(
+                  children: [
+                    LastMemories(
+                      icon: Icons.filter_center_focus,
+                      title: 'Skydiven',
+                      tag: 'Tag',
+                      paidMoney: '13€',
+                      date: DateTime(2017, 1, 1),
+                    ),
+                    LastMemories(
+                      icon: Icons.filter_center_focus,
+                      title: 'Skydiven 2',
+                      tag: 'Tag',
+                      paidMoney: '13€',
+                      date: DateTime(2017, 2, 1),
+                    ),
+                    LastMemories(
+                      icon: Icons.filter_center_focus,
+                      title: 'Skydiven 3',
+                      tag: 'Tag',
+                      paidMoney: '13€',
+                      date: DateTime(2017, 3, 1),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
