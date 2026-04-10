98a4a7b309ec4c30a79de4db00f4cfa817fdd851
fairytaib <fairytaib@gmail.com>
2026-04-10 16:37:26 +0200

    Safety commit

---
+++ lib/features/deen/presentation/deen_screen.dart
@@ -0,0 +1,20 @@
+// Flutter packages
+import 'package:flutter/material.dart';
+
+//Css
+import 'package:husband/shared/style_constants.dart';
+
+class DeenScreen extends StatefulWidget {
+  const DeenScreen({super.key});
+
+  @override
+  State<DeenScreen> createState() => _DeenScreenState();
+}
+
+class _DeenScreenState extends State<DeenScreen> {
+  int _selectedTab = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
